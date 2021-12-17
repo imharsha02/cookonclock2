4f8b397955cbc9f55515ba304013d8cf4e3758fb
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2021-12-17 22:21:45 +0530

    works data

--- pages/RecipeList.js
+++ pages/RecipeList.js
@@ -1,8 +1,27 @@
 import { withRouter } from "next/router";
+import {useState, useEffect} from 'react'
+
+
 function RecipeList(props) {
+    const [res, setRes] = useState({});
+    const getData = async() => {
+        fetch("https://api.edamam.com/api/recipes/v2?type=public&q=egg&app_id=67e0e669&app_key=401c79fd1cbca14687587eea063de9ae&ingr=1-8")
+        .then((response)=> {
+            return response.json()
+        }).then(data => {
+            console.log(data)
+            setRes(data)
+        })
+        
+    }
+    useEffect(() => {
+        getData()
+    }, [])
     return (
         <>
-            <style jsx>
+            {res == {} ? (
+                <>
+                <style jsx>
                 {
                     `
                     h1{
@@ -14,6 +33,12 @@ function RecipeList(props) {
                 }
             </style>
             <h1>{props.router.query.q}</h1>
+                </>
+            ): (
+                <div>
+                    yo{res.hits&& res.hits.length>0 && res.hits.length }
+                </div>
+            )}
         </>
     )
 }

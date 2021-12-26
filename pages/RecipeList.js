81d8ce968280a73fb9989f9bdca8cc997ccbd1a0
gmahima <mahimagangavarapu@gmail.com>
2021-12-26 14:33:07 +0530

    lists ingredients

--- pages/RecipeList.js
+++ pages/RecipeList.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react'
 
 function RecipeList(props) {
     const [res, setRes] = useState({});
+    const [error, setError] = useState('');
     const getData = async() => {
         fetch("https://api.edamam.com/api/recipes/v2?type=public&q=egg&app_id=67e0e669&app_key=401c79fd1cbca14687587eea063de9ae&ingr=1-8")
         .then((response)=> {
@@ -11,32 +12,37 @@ function RecipeList(props) {
         }).then(data => {
             console.log(data)
             setRes(data)
-        })
-        
+        }).catch(error => {
+            setError(`${error}`)
+        })       
     }
     useEffect(() => {
         getData()
     }, [])
     return (
         <>
-            {res == {} ? (
-                <>
-                <style jsx>
-                {
-                    `
-                    h1{
-                        text-align:center;
-                        font-size:2.5rem;
-                        margin:0px 0px 2px 0px;
-                    }
-                `
-                }
-            </style>
+            {Object.keys(res).length == 0 ? (
+                error == '' ? (
+                    <>
             <h1>{props.router.query.q}</h1>
+            <p>loading please wait</p>
                 </>
+                ) : (
+                    <p>{error}</p>
+                )
             ): (
                 <div>
-                    yo{res.hits&& res.hits.length>0 && res.hits.length }
+                    {console.log(res)}
+                    {res.hits&& res.hits.length>0 && (
+                        <div>
+                            {res.hits.map(obj => <div>
+                                {obj.recipe.ingredients.map(ing => <div>
+                                    <h1>{ing.food}</h1>
+                                    
+                                </div>)}
+                            </div>)}
+                        </div>
+                    ) }
                 </div>
             )}
         </>

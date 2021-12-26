7bf41eb23507fdca1cc2fd14e2e8762170ce9d96
gmahima <mahimagangavarapu@gmail.com>
2021-12-26 15:04:20 +0530

    show in card

--- pages/RecipeList.js
+++ pages/RecipeList.js
@@ -1,6 +1,6 @@
 import { withRouter } from "next/router";
 import {useState, useEffect} from 'react'
-
+import { Card, Icon, Image, Table, Header } from 'semantic-ui-react'
 
 function RecipeList(props) {
     const [res, setRes] = useState({});
@@ -35,12 +35,47 @@ function RecipeList(props) {
                     {console.log(res)}
                     {res.hits&& res.hits.length>0 && (
                         <div>
-                            {res.hits.map(obj => <div>
+                            {/* {res.hits.map(obj => <div>
+                                <h1>{obj.recipe.label}</h1>
                                 {obj.recipe.ingredients.map(ing => <div>
-                                    <h1>{ing.food}</h1>
+                                    <h4>{ing.food}</h4>
                                     
                                 </div>)}
-                            </div>)}
+                            </div>)} */}
+                            {res.hits.map(obj => <Card>
+                                <Image src={obj.recipe.image} wrapped ui={false} />
+                                <Card.Content>
+                                <Card.Header>{obj.recipe.label}</Card.Header>
+                                <Card.Description>
+                                    <Table basic="very" celled collapsing>
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell>Ingredient</Table.HeaderCell>
+                                            <Table.HeaderCell>Quantity</Table.HeaderCell>
+                                        </Table.Row>
+                                    </Table.Header>
+                                    <Table.Body>
+                                    
+                                        {obj.recipe.ingredients.map(ing => 
+                                         <Table.Row>
+                                                    <Table.Cell>
+                                                    <Header as='h4' image>
+                                                        <Image src={ing.image} rounded size='mini' />
+                                                        <Header.Content>
+                                                        {ing.food}
+                                                        {ing.measure && ing.measure != "<unit>" && <Header.Subheader>in {ing.measure}</Header.Subheader>}
+                                                        </Header.Content>
+                                                    </Header>
+                                                    </Table.Cell>
+                                                    <Table.Cell>{ing.quantity == 0 ? "as required" : ing.quantity}</Table.Cell>
+                                            </Table.Row>   
+                                        )}
+                                    </Table.Body>
+                                    </Table>
+                                </Card.Description>
+                                
+                                </Card.Content>
+                            </Card>)}
                         </div>
                     ) }
                 </div>
